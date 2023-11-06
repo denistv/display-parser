d1fe16e84953e5ef260e9c06e265b579f0fb3388
Denis Tv <denistv777@gmail.com>
2023-11-06 16:11:14 +0300

    implement pkg/logger std methods

--- pkg/logger/std_wrapper.go
+++ pkg/logger/std_wrapper.go
@@ -1,8 +1,23 @@
 package logger
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"strings"
+)
 
-func NewStdWrapper() *STDWrapper{
+type LogLevel string
+
+const (
+	Debug LogLevel = "Debug"
+	Info  LogLevel = "Info"
+	Warn  LogLevel = "Warn"
+	Error LogLevel = "Error"
+	Panic LogLevel = "Panic"
+	Fatal LogLevel = "Fatal"
+)
+
+func NewStdWrapper() *STDWrapper {
 	return &STDWrapper{
 		logger: log.Default(),
 	}
@@ -11,4 +26,43 @@ func NewStdWrapper() *STDWrapper{
 // STDWrapper обертка для логгера из стандартной библиотеки Go
 type STDWrapper struct {
 	logger *log.Logger
+}
+
+func (s *STDWrapper) Debug(msg string, fields ...Field) {
+	s.logger.Printf(newMsg(Debug, msg, fields...))
+}
+
+func (s *STDWrapper) Info(msg string, fields ...Field) {
+	s.logger.Printf(newMsg(Info, msg, fields...))
+}
+
+func (s *STDWrapper) Warn(msg string, fields ...Field) {
+	s.logger.Printf(newMsg(Warn, msg, fields...))
+}
+
+func (s *STDWrapper) Error(msg string, fields ...Field) {
+	s.logger.Printf(newMsg(Error, msg, fields...))
+}
+
+func (s *STDWrapper) Panic(msg string, fields ...Field) {
+	s.logger.Printf(newMsg(Panic, msg, fields...))
+}
+
+func (s *STDWrapper) Fatal(msg string, fields ...Field) {
+	s.logger.Printf(newMsg(Fatal, msg, fields...))
+}
+
+func newMsg(level LogLevel, msg string, fields ...Field) string {
+	sb := strings.Builder{}
+
+	sb.WriteString(fmt.Sprintf("[%s] %s", level, msg))
+
+	if len(fields) > 0 {
+		sb.WriteString(" ")
+		for _, v := range fields {
+			sb.WriteString(fmt.Sprintf(`{key: "%s", value: %q}`, v.Key, v.Value))
+		}
+	}
+
+	return sb.String()
 }
