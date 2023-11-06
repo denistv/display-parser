f1d3b9d570c5afedb365c5dcef3f97b0339d590c
Denis Tv <denistv777@gmail.com>
2023-11-06 15:12:33 +0300

    pkg/logger implement zap methods

--- pkg/logger/std_wrapper.go
+++ pkg/logger/std_wrapper.go
@@ -1,10 +1,14 @@
 package logger
 
+import "log"
+
 func NewStdWrapper() *STDWrapper{
-	return &STDWrapper{}
+	return &STDWrapper{
+		logger: log.Default(),
+	}
 }
 
 // STDWrapper обертка для логгера из стандартной библиотеки Go
 type STDWrapper struct {
-
+	logger *log.Logger
 }
--- pkg/logger/zap_wrapper.go
+++ pkg/logger/zap_wrapper.go
@@ -1,3 +1,5 @@
+// Package logger
+// TODO вынести *_wrapper в отдельные подпакеты
 package logger
 
 import "go.uber.org/zap"
@@ -11,4 +13,50 @@ func NewZapWrapper(zap *zap.Logger) *ZapWrapper {
 // ZapWrapper обертка для Zap
 type ZapWrapper struct{
 	zap *zap.Logger
+}
+
+func (z *ZapWrapper) Debug(msg string, fields ...Field) {
+	z.zap.Debug(msg, newZapFields(fields...)...)
+}
+
+func (z *ZapWrapper) Info(msg string, fields ...Field) {
+	z.zap.Info(msg, newZapFields(fields...)...)
+}
+
+func (z *ZapWrapper) Warn(msg string, fields ...Field) {
+	z.zap.Warn(msg, newZapFields(fields...)...)
+}
+
+func (z *ZapWrapper) Error(msg string, fields ...Field) {
+	z.zap.Error(msg, newZapFields(fields...)...)
+}
+
+func (z *ZapWrapper) Panic(msg string, fields ...Field) {
+	z.zap.Panic(msg, newZapFields(fields...)...)
+}
+
+func (z *ZapWrapper) Fatal(msg string, fields ...Field) {
+	z.zap.Fatal(msg, newZapFields(fields...)...)
+}
+
+func (z *ZapWrapper) Sync() error {
+	return z.zap.Sync()
+}
+
+// NewZapField реализована не самым удачным образом. Когда-нибудь вернусь к этому вопросу (или не вернусь). Сейчас это не принципиальный вопрос.
+func newZapField(f Field) zap.Field {
+	return zap.Field{
+		Key: f.Key,
+		Interface: f.Value,
+	}
+}
+
+func newZapFields(fs ...Field) []zap.Field{
+	out := make([]zap.Field, 0, len(fs))
+
+	for _, f := range fs {
+		out = append(out, newZapField(f))
+	}
+
+	return out
 }
